8a016bba62f44470c84f1a173abb4fec50001dd1
Blessing <blessing@Blessings-Air.broadband>
2022-01-04 13:15:03 +0000

    initialised bankaccount class with a starting balance of 0 so client can create bank account

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -2,4 +2,11 @@ require './lib/bank_account'
 
 describe BankAccount do
 
+  describe '#initialize' do
+    it 'should have a default balance of 0' do
+      account = BankAccount.new
+      expect(account.balance).to eq 0
+    end
+  end
+
 end
