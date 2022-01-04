dba7ff370701902e77fba6a81577e34f9e11f870
Blessing <blessing@Blessings-Air.broadband>
2022-01-04 14:36:56 +0000

    added a test so that the client can make a withdrawal out of the account - red

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -10,11 +10,21 @@ describe BankAccount do
   end
 
   describe '#deposit' do
-  it 'should increase the bank balance by the deposited amount' do
-    account = BankAccount.new
-    account.deposit(5000)
-    expect(account.balance).to eq 5000
+    it 'should increase the bank balance by the deposited amount' do
+        account = BankAccount.new
+        account.deposit(5000)
+        expect(account.balance).to eq 5000
+    end
   end
-end
+
+  describe '#withdraw' do
+    it 'should decrease the bank balance by the withdrawn amount' do
+        account = BankAccount.new
+        account.deposit(5000)
+        account.withdraw(500)
+        account.deposit(20)
+        expect(account.balance).to eq 4520
+    end
+  end
 
 end
