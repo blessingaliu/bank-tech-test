98cdcbc278274ce26d1fb554625ddf5ca6f20870
Blessing <blessing@Blessings-Air.broadband>
2022-01-04 14:42:20 +0000

    passed the test so that the client can make a withdrawal out of the account - green

--- spec/feature_test.rb
+++ spec/feature_test.rb
@@ -15,3 +15,11 @@ def deposit_money
     account.deposit(5000)
     p account.balance
 end
+
+def withdraw_money
+    account = BankAccount.new
+    account.deposit(5000)
+    account.deposit(500)
+    account.withdraw(20)
+    p account.balance
+  end
